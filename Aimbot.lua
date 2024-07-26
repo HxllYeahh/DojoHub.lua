cf02fd225e2d4e2e624215ea1e47e255ecfbd780
HxllYeahh <hxllyeahh@gmail.com>
2024-07-26 18:57:46 +0800

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -1,4 +1,4 @@
-_G.HeadSize = 10
+_G.HeadSize = 12
 _G.Disabled = true
  
 game:GetService('RunService').RenderStepped:connect(function()
@@ -8,7 +8,7 @@ if v.Name ~= game:GetService('Players').LocalPlayer.Name then
 pcall(function()
 v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
 v.Character.HumanoidRootPart.Transparency = 0.7
-v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Dark red")
+v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Lime green")
 v.Character.HumanoidRootPart.Material = "Neon"
 v.Character.HumanoidRootPart.CanCollide = false
 end)
