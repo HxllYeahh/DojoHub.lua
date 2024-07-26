9eb93d7a02ff5faec22a70ee9bb8e96843872d84
HxllYeahh <hxllyeahh@gmail.com>
2024-07-26 19:25:07 +0800

    Update DojoEsp1.lua

--- DojoEsp1.lua
+++ DojoEsp1.lua
@@ -1,1 +1,6 @@
+if game.PlaceId == 12137249458 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HxllYeahh/DojoHub.lua/main/DojoEsp.lua"))()
+else
+game.Players.LocalPlayer:Kick("wrong game buddy")
+end
+end
